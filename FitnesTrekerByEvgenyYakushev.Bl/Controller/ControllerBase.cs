63e97e8527046962ff517c095cd6ffd6a01f2551
Yakushev Andrey <561754@mail.ru>
2020-01-11 23:25:08 +0300

    Ещё не доконца, поэтому докончить 3 видео с сериализации

---
+++ FitnesTrekerByEvgenyYakushev.Bl/Controller/ControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnesTrekerByEvgenyYakushev.Bl.Controller
+{
+    abstract class ControllerBase
+    {
+        protected void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Users);
+            }
+        }
+
+
+    }
+    }
+}
